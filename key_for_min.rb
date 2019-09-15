81d3fa51dc70bad901b33c79d29e3f0fa48f8af7
Jonah Brown <jswish02@gmail.com>
2019-09-15 22:07:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+food = {:candy => 20, :chicken => 27, :fish => 25}
 
+key_for_min_value(food)
 end
